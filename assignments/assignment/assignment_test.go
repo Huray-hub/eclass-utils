711f6a05dc21ebf1bfbd2219b90fa72da6bd74c6
Huray-hub <panos.skiadas12@gmail.com>
2022-12-03 18:39:34 +0200

    Updated deadline parser to adapt to new version

---
+++ assignments/assignment/assignment_test.go
@@ -0,0 +1,50 @@
+package assignment
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseNearDeadline_Tomorrow(t *testing.T) {
+	// Arrange
+	location, err := time.LoadLocation("Europe/Athens")
+	if err != nil {
+		t.Errorf("failed to load location %v", err)
+	}
+
+	deadlineStr := "αύριο - 11:59 μ.μ.(απομένουν 1 ημέρα 3 ώρες 8 λεπτά)"
+	expectedDeadline := time.Date(2022, 12, 4, 23, 59, 0, 0, location)
+
+	// Act
+	deadline, err := parseDeadline(deadlineStr, location)
+	if err != nil {
+		t.Errorf("failed to parse deadline: '%v'", deadline)
+	}
+
+	// Assert
+	if !deadline.Equal(expectedDeadline) {
+		t.Errorf("Expected: %s, Actual: %s", expectedDeadline, deadline)
+	}
+}
+
+func TestParseNormalDeadline(t *testing.T) {
+	// Arrange
+	location, err := time.LoadLocation("Europe/Athens")
+	if err != nil {
+		t.Errorf("failed to load location %v", err)
+	}
+
+	deadlineStr := "Τετάρτη 21 Δεκεμβρίου 2022 - 11:59 μ.μ.(απομένουν 19 ημέρες 3 ώρες 8 λεπτά)"
+	expectedDeadline := time.Date(2022, 12, 21, 23, 59, 0, 0, location)
+
+	// Act
+	deadline, err := parseDeadline(deadlineStr, location)
+	if err != nil {
+		t.Errorf("failed to parse deadline: '%v'", deadline)
+	}
+
+	// Assert
+	if !deadline.Equal(expectedDeadline) {
+		t.Errorf("Expected: %v, Actual: %v", expectedDeadline, deadline)
+	}
+}
---
+++ assignments/assignment/timeParser.go
@@ -0,0 +1,138 @@
+package assignment
+
+import (
+	"errors"
+
+	"strings"
+	"time"
+)
+
+var dayNamesGR = map[string]string{
+	"Δευτέρα":   "Monday",
+	"Τρίτη":     "Tuesday",
+	"Τετάρτη":   "Wednesday",
+	"Πέμπτη":    "Thursday",
+	"Παρασκευή": "Friday",
+	"Σάββατο":   "Saturday",
+	"Κυριακή":   "Sunday",
+}
+
+var monthNamesGenitiveGR = map[string]string{
+	"Ιανουαρίου":  "January",
+	"Φεβρουαρίου": "February",
+	"Μαρτίου":     "March",
+	"Απριλίου":    "April",
+	"Μαΐου":       "May",
+	"Ιουνίου":     "June",
+	"Ιουλίου":     "July",
+	"Αυγούστου":   "August",
+	"Σεπτεμβρίου": "September",
+	"Οκτωβρίου":   "October",
+	"Νοεμβρίου":   "November",
+	"Δεκεμβρίου":  "December",
+}
+
+var periodsGR = map[string]string{
+	"π.μ.": "am",
+	"μ.μ.": "pm",
+}
+
+func parseTime(dateRaw string, location *time.Location) (*time.Time, error) {
+	if strings.Contains(dateRaw, "αύριο") || strings.Contains(dateRaw, "μεθαύριο") {
+		return parseNearTime(dateRaw, location)
+	}
+
+	return parseNormalDate(dateRaw, location)
+}
+
+// parseNearTime parses the following formats:
+// "αύριο - 11:59 μ.μ.(απομένουν 1 ημέρα 3 ώρες 8 λεπτά)"
+// "μεθαύριο - 11:59 μ.μ.(απομένουν 2 ημέρες 3 λώρες 8 λεπτά)"
+func parseNearTime(nearDate string, location *time.Location) (*time.Time, error) {
+	dateOnly, err := parseNearDateOnly(nearDate)
+	if err != nil {
+		return nil, err
+	}
+
+	timeOnly, err := parseTimeOnly(nearDate)
+	if err != nil {
+		return nil, err
+	}
+
+	fullTime := time.Date(
+		dateOnly.Year(),
+		dateOnly.Month(),
+		dateOnly.Day(),
+		timeOnly.Hour(),
+		timeOnly.Minute(),
+		timeOnly.Second(),
+		0,
+		location,
+	)
+
+	return &fullTime, nil
+}
+
+func parseNearDateOnly(date string) (*time.Time, error) {
+	dateOnly := time.Now()
+
+	switch {
+	case strings.Contains(date, "αύριο"):
+		dateOnly = dateOnly.AddDate(0, 0, 1)
+	case strings.Contains(date, "μεθαύριο"):
+		dateOnly = dateOnly.AddDate(0, 0, 2)
+	default:
+		return nil, errors.New("invalid value")
+	}
+
+	return &dateOnly, nil
+}
+
+func parseTimeOnly(timeRaw string) (*time.Time, error) {
+	timeParts := strings.Split(timeRaw, " ")
+	periodGR := strings.Split(timeParts[3], "(")[0]
+
+	timeRaw = timeParts[2] + periodsGR[periodGR]
+
+	time, err := time.Parse("15:04pm", timeRaw)
+	if err != nil {
+		return nil, err
+	}
+
+	return &time, nil
+
+}
+
+// parseNormalDate will parse
+func parseNormalDate(s string, location *time.Location) (*time.Time, error) {
+	timeRaw, _, found := strings.Cut(s, "(")
+	if !found {
+		return nil, errors.New("could not cut string by '(' :" + timeRaw)
+	}
+
+	timeRaw = translateTimeGrEn(timeRaw)
+
+	t, err := time.ParseInLocation("Monday 2 January 2006 - 15:04 pm", timeRaw, location)
+	if err != nil {
+		return nil, err
+	}
+
+	return &t, nil
+}
+
+// TranslateTimeGrEn translates Greek string time of format
+// "Τετάρτη 21 Δεκεμβρίου 2022 - 11:59 μ.μ." in English
+func translateTimeGrEn(dt string) string {
+	parts := strings.Split(dt, " ")
+
+	dayGR := &parts[0]
+	dt = strings.Replace(dt, *dayGR, dayNamesGR[*dayGR], 1)
+
+	monthGR := &parts[2]
+	dt = strings.Replace(dt, *monthGR, monthNamesGenitiveGR[*monthGR], 1)
+
+	periodGR := &parts[6]
+	dt = strings.Replace(dt, *periodGR, periodsGR[*periodGR], 1)
+
+	return dt
+}
